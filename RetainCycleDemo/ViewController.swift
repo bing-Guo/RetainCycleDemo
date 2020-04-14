5c248bc7d453a86e25bbbafe907b49683fcbe675
bing <bindaguo@gmail.com>
2020-04-14 12:00:15 +0800

    first commit

--- RetainCycleDemo/ViewController.swift
+++ RetainCycleDemo/ViewController.swift
@@ -1,20 +1,51 @@
-//
-//  ViewController.swift
-//  RetainCycleDemo
-//
-//  Created by Bing Guo on 2020/4/14.
-//  Copyright © 2020 Bing Guo. All rights reserved.
-//
-
 import UIKit
+
+class User {
+    let name: String
+    private(set) var phones: [Phone] = []
+    
+    init(name: String) {
+        self.name = name
+        print("User \(name) was initialized")
+    }
+    
+    deinit {
+        print("Deallocating user named: \(name)")
+    }
+    
+    func add(phone: Phone) {
+        phones.append(phone)
+        phone.owner = self
+    }
+}
+
+class Phone {
+    let model: String
+    var owner: User?
+    
+    init(model: String) {
+        self.model = model
+        print("Phone \(model) was initialized")
+    }
+    
+    deinit {
+        print("Deallocating phone named: \(model)")
+    }
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        runScenario()
     }
 
+    func runScenario() {
+        let user = User(name: "John")
+        let iPhone = Phone(model: "iPhone Xs")
+        user.add(phone: iPhone)
+    }
 
 }
 
